48fd8f94a8c0b6eafd63ff742cdc003a204b3ebc
gboxwell <gboxwell@numerex.com>
2024-10-02 10:31:54 -0600

    update to handle no report type

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,9 @@ composer install
 
 export PATH=$PATH:$GITHUB_ACTION_PATH/vendor/bin
 
-./vendor/bin/phpcs -v $PATHS --standard=PHPCompatibility --runtime-set testVersion $TEST_VERSIONS --report $REPORT_TYPE
+if [ ! -z "$REPORT_TYPE" -a  "$REPORT_TYPE"!=" "]; then
+./vendor/bin/phpcs -v $PATHS --standard=PHPCompatibility --runtime-set testVersion $TEST_VERSIONS --report=$REPORT_TYPE
+else
+./vendor/bin/phpcs -v $PATHS --standard=PHPCompatibility --runtime-set testVersion $TEST_VERSIONS
+fi
+
